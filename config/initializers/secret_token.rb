5d9fcf54cd83a148b7f6a64d23bf30cf282f951a
Karine Almeida <karine.silalmeida@gmail.com>
2012-09-02 21:11:10 -0300

    Iniciando projeto blog em rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeiaDeBlog::Application.config.secret_token = '10a5dc17073dce18bf2e5eec5734aa2ceaeb0925b56f3e418ebd537a6a37e8fb4c4d56407a7e40bbad05ea1ed70cbe0715db5a4026fc4de8eeece1ab35d6e10c'
